31b037de8f235830dce8db1631f1870594ab3db2
titanturtle90 <markcconan@gmail.com>
2026-08-27 18:16:04 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "grocery-tracker-v2";
+const CACHE_NAME = "nexpire-v3";
 const PRECACHE_URLS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
