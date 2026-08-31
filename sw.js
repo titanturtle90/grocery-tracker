77966d962ccf9b628a176ad1fe9467380ce2f91b
titanturtle90 <markcconan@gmail.com>
2026-08-31 18:57:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nexpire-v5";
+const CACHE_NAME = "nexpire-v7";
 const PRECACHE_URLS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
